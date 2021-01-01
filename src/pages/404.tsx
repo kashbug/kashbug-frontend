bec7cce1d6495c3c30b22e45fd9a31a4529394a5
kashbug <kashbug.manager@gmail.com>
2021-01-01 15:18:53 +0900

    create 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,29 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography } from '@material-ui/core';
 const NotFound = () => {
+    const useStyles = makeStyles({
+        centerWrap: {
+            position: 'fixed',
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+        },
+        
+    });
+    const classes = useStyles();
+
     return (
-        <h1>NotFound</h1>
+        <>
+            <div className={classes.centerWrap}>
+                <Typography variant="h4" component="p">
+                SORRY, PAGE NOT FOUND
+                </Typography>
+            </div>
+        </>
     );
 };
 
