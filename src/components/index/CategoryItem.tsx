845747f4560078d1b98f03c7c7d0814956fb3260
joon <joonij93@gmail.com>
2021-01-09 17:07:58 +0900

    20210109

--- src/components/index/CategoryItem.tsx
+++ src/components/index/CategoryItem.tsx
@@ -1,24 +1,16 @@
-
 import styled from 'styled-components';
-// mui
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
 
-const ItemWrap = styled.div`
+const ItemWrap = styled.li`
     display: inline-block;
-    padding: 8px;
+    padding: 16px;
+    &:first-child {
+        padding-left: 0;
+    }
+    &:last-child {
+        padding-right: 0;
+    }
 `;
-const Logo = styled.div`
-    display: flex;
-    padding: 8px;
-    width: 200px;
-    height: 200px;
-    align-items: flex-end;
-    justify-content: flex-end;
-    background-image: url(${props => props.url});
-    border-radius: 16px;
-`;
-const TemporaryLogo = styled.div`
+const LogoBox = styled.div`
     display: flex;
     padding: 8px;
     width: 200px;
@@ -28,40 +20,31 @@ const TemporaryLogo = styled.div`
     background-color: #491651;
     border-radius: 16px;
 `;
+const Logo = styled.img`
 
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        closingDate: {
-            color: '#000000',
-            fontWeight: 500,
-            textShadow: '-1px 0 #ffffff, 0 1px #ffffff, 1px 0 #ffffff, 0 -1px #ffffff'
-        },
-        projectName: {
-            display: 'flex',
-            justifyContent: 'center',
-            padding: '8px 0',
-        },
-    }),
-);
+`;
+const ClosingDate = styled.p`
+    color: #000000;
+    font-weight: 500;
+    text-shadow: -1px 0 #ffffff, 0 1px #ffffff, 1px 0 #ffffff, 0 -1px #ffffff;
+    font-size: 0.875rem;
+`;
+const ProjectName = styled.p`
+    display: flex;
+    justify-content: center;
+    padding: 8px 0;
+    font-size: 1rem;
+`;
 
 const CategoryItem = ({...props}) => {
-    const classes = useStyles();
     return (
         <>
             <ItemWrap>
-                <TemporaryLogo>
-                    <Typography variant="body2" component="p" className={classes.closingDate}>
-                    {props.closingDate}
-                    </Typography>
-                </TemporaryLogo>
-                {/* <Logo url={props.projectLogo}>
-                    <Typography variant="body2" component="p" className={classes.closingDate}>
-                    {props.closingDate}
-                    </Typography>
-                </Logo> */}
-                <Typography variant="body1" component="p" className={classes.projectName}>
-                {props.projectName}
-                </Typography>
+                <LogoBox>
+                    <img src={props.projectLogo} alt="Logo"/>
+                    <ClosingDate>{props.closingDate}</ClosingDate>
+                </LogoBox>
+                <ProjectName>{props.projectName}</ProjectName>
             </ItemWrap>
         </>
     );
--- src/containers/index/IndexContainer.tsx
+++ src/containers/index/IndexContainer.tsx
@@ -1,32 +1,33 @@
 import React, { useState } from 'react';
-
-// mui
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import styled from 'styled-components';
 import CategoryItem from '../../components/index/CategoryItem';
 
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        container: {
-            marginTop: '60px',
-        }
-    }),
-);
+const IndexListWrap = styled.ul`
+    margin-top: 60px;
+`;
+
+interface indexItems {
+    idx: Number;
+    projectLogo: string;
+    projectName: string;
+    price: Number;
+    closingDate: string;
+}
 
 const IndexContainer = () => {
-    const [indexItems, setIndexItems] = useState([
+    const [indexItems, setIndexItems] = useState<indexItems[]>([
         {idx: 0, projectLogo: '../#', projectName: '야구게임', price: 30000, closingDate: '2021-12-31'},
         {idx: 1, projectLogo: '../#', projectName: '축구게임', price: 20000, closingDate: '2021-12-30'},
         {idx: 2, projectLogo: '../#', projectName: '배구게임', price: 10000, closingDate: '2021-12-29'},
         {idx: 3, projectLogo: '../#', projectName: '농구게임', price: 20000, closingDate: '2021-12-28'},
     ]);
 
-    const classes = useStyles();
     return (
-        <div className={classes.container}>
+        <IndexListWrap>
             {indexItems.map((row, index) =>
-                <CategoryItem key={index} projectLogo={row.projectLogo} projectName={row.projectName} price={row.price} closingDate={row.closingDate} />
+                <CategoryItem key={`${index}_key`} projectLogo={row.projectLogo} projectName={row.projectName} price={row.price} closingDate={row.closingDate} />
             )}
-        </div>
+        </IndexListWrap>
     );
 };
 
