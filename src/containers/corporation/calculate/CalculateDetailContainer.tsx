24db36964bbf6693a935f2c6c5ac1d69f6965d29
joon <joonij93@gmail.com>
2021-02-09 14:00:01 +0900

    [calculte-detail] 정산 상세 페이지 개발

--- src/containers/corporation/calculate/CalculateDetailContainer.tsx
+++ src/containers/corporation/calculate/CalculateDetailContainer.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 // mui
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
     table: {
@@ -19,13 +20,31 @@ const useStyles = makeStyles((theme) => ({
     content: {
         flex: '1 1',
     },
+    submitBtn: {
+        margin: '0 8px',
+        '&:first-child': {
+            marginLeft: '0'
+        },
+        '&:last-child': {
+            marginRight: '0'
+        }
+    }
 }));
 
 const ContentBox = styled.div`
     display: flex;
+    justify-content: center;
     padding-bottom: 24px;
 `;
-const bug = styled.img`
+const ImgBox = styled.div`
+    display: flex;
+    padding-bottom: 24px;
+    flex-wrap: wrap;
+    justify-content: space-between;
+`;
+const BugImg = styled.img`
+    width: calc(50% - 16px);
+    padding-bottom: 8px;
 `;
 
 interface calculateItem {
@@ -63,7 +82,9 @@ const CalculateDetailContainer = () => {
             </ContentBox>
             <ContentBox>
                 <TextField className={classes.leftContent} label="프로젝트" value={calculateItem.project} disabled/>
-                <TextField className={classes.rightContent} label="카테고리" value={calculateItem.bugType} disabled/>
+                {calculateItem.bugType === 0 && <TextField className={classes.rightContent} label="카테고리" value="오타" disabled/>}
+                {calculateItem.bugType === 1 && <TextField className={classes.rightContent} label="카테고리" value="기능오류" disabled/>}
+                {calculateItem.bugType === 2 && <TextField className={classes.rightContent} label="카테고리" value="화면배치" disabled/>}
             </ContentBox>
             <ContentBox>
                 <TextField className={classes.leftContent} label="제목" value={calculateItem.title} disabled/>
@@ -72,10 +93,14 @@ const CalculateDetailContainer = () => {
             <ContentBox>
                 <TextField className={classes.content} label="내용" value={calculateItem.content} multiline disabled/>
             </ContentBox>
+            <ImgBox>
+                <BugImg src="/img_notfound.png" alt="#" />
+                <BugImg src="/img_notfound.png" alt="#" />
+                <BugImg src="/img_notfound.png" alt="#" />
+            </ImgBox>
             <ContentBox>
-                <img src="/img_notfound.png" alt="#" />
-                <img src="/img_notfound.png" alt="#" />
-                <img src="/img_notfound.png" alt="#" />
+                <Button className={classes.submitBtn} variant="contained" color="primary">입금완료</Button>
+                <Button className={classes.submitBtn} variant="contained" color="secondary">취소</Button>
             </ContentBox>
         </>
     );
